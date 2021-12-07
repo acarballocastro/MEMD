0bc752f8392a7bc7cae219ef8c4fadb3769fab8c
yuhangliang <91143269+yuhangliang@users.noreply.github.com>
2021-12-08 00:05:22 +0100

    Update 4.1.Descriptiva_univariante.r

--- outputs/4.1.Descriptiva_univariante.r
+++ outputs/4.1.Descriptiva_univariante.r
@@ -14,7 +14,7 @@ v<-e3.cor$v
 for(i in v$categoric){
   print(ggplot(d.e, aes(x=d.e[[i]])) + geom_bar(stat = "count", fill='orange',na.rm = T)+
           theme_minimal() +
-          labs(title = "Histogram",x=i,main=i))
+          labs(title = i,x=i))
   print(summary(d.e[[i]]))
 }
 
