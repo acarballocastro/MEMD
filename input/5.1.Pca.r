f00d9395b85646f445e9220b4d75ebd3097ddd4f
yuhangliang <91143269+yuhangliang@users.noreply.github.com>
2021-12-07 21:51:54 +0100

    Update and rename 5.Pca.r to 5.1.Pca.r

---
+++ input/5.1.Pca.r
@@ -0,0 +1,29 @@
+###############################################
+# T?tulo: 5.1. PCA
+# Autor: Aleix
+# Fecha: 30/11/21
+
+# Descripci?n: PRINCIPAL COMPONENT ANALYSIS (PCA)
+###############################################
+
+if(!exists('e3.cor')) source('input/3.2.Correlacion.tract.r')
+d.e<-e3.cor$d
+v<-e3.cor$v
+
+# cargamos los paquetes que usaremos
+for(i in c("corrplot","PerformanceAnalytics", "FactoMineR", "factoextra")) require(i,character.only = T)
+
+# PCA
+# Si queremos suprimir el outlier d.e[v$integer][-173,]
+nl<-length(v$numeric)
+res.pca<-PCA(d.e[v$numeric],graph = F,ncp=nl)
+
+ni<-which(res.pca$eig[,3]>80)[1]
+res.pca<-PCA(d.e,graph = F,ncp=ni,quali.sup =which(names(d.e)%in%v$categoric))
+
+e5.pca<-new.env()
+e5.pca$d<-d.e
+e5.pca$v<-v
+e5.pca$pca<-res.pca
+
+
