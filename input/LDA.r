dcc1f95d88300fe0bb69280254334eb167d46a94
yuhangliang <91143269+yuhangliang@users.noreply.github.com>
2021-12-15 22:27:37 +0100

    Update LDA.r

--- input/LDA.r
+++ input/LDA.r
@@ -1,4 +1,7 @@
-if(!exists('e3.cor')) source('input/3.3.Correlacion_tract.r')
+if(!exists('filename')) filename<- 'hotel_bookings.csv'
+if(!exists('path')) path<-strsplit(shell('dir ..\\TMD.Rmd /s',intern = T)[4],' ')[[1]][4]
+setwd(path)
+if(!exists('e3.cor')) source('input/1_Preprocessing.r')
 d.e<-e3.cor$d
 v<-e3.cor$v
 
@@ -25,7 +28,7 @@ accu<-function(data,mod,y,pred.select,table=F,...){
 }
 
 # lda falla cuando variable tenga factor sin observacion: reserved_room_type
-d.lda<-d.e
+d.lda<-d.e[c(v$numeric,v$categoric)]
 d.lda$reserved_room_type<-factor(as.character(d.e$reserved_room_type))
 # Construir el model con data conjunta
 mod.lda<-lda(data=d.lda,is_canceled~.)
@@ -45,23 +48,25 @@ print(acuracy)
 # no tenga mucha diferencia entre acuracy del test y train con el mod2, por lo tanto no hay overfiting
 
 # Grafica de roc
-p.train <- predict(mod.lda2)
+p.train<-predict(mod.lda2,d.lda.train)
 p.test<-predict(mod.lda2,d.lda.test)
-print(roc.plot(d.lda.train$is_canceled == "1", p.train$posterior[, 2])$roc.vol )
 print(roc.plot(d.lda.test$is_canceled == "1", p.test$posterior[, 2])$roc.vol )
 
 # Se busca el corte de probabilidad para tener mejor acuracy
 pes<-0.5
-f<-function(pes){
-  cla<-sapply(p.test$posterior[,1], function(x) ifelse(x>pes,0,1))
-  tab<-xtabs(~cla+d.lda.test$is_canceled)
+
+f<-function(pes,tabla=F,prob,class=levels(d.e$is_canceled) , data=d.lda.test$is_canceled){
+  cla<-sapply(prob, function(x) factor(ifelse(x>pes,class[1],class[2])))
+  tab<-xtabs(~cla+data)
   acu<-sum(diag(tab))/sum(tab)
+  if(tabla) return(list(matrix=tab,accuracy=acu))
   return(acu)
 }
+
 a<-numeric(99)
-for(i in 1:99) a[i]<-f(i/100)
+for(i in 1:99) a[i]<-f(i/100,prob = p.test$posterior[,1])
 (pes<-which.max(a)/100)
-print(f(pes))
+print(f(pes,table = T,prob = p.test$posterior[,1]))
 vmedi<-which.min(abs(p.test$posterior[,1]-pes))
 # resulta que apartir de 0.53 se obtiene mejor acuracy
 
@@ -78,6 +83,8 @@ polygon(x=c(-500,2000,2000,-500),y=c(5,5,corte,corte),
 polygon(x=c(-500,2000,2000,-500),y=c(-5,-5,corte,corte),
         col=col0,density = 20)
 
+p.test$posterior[204,]
+
 # Como que solo hay una unica LDA, resulta mejor un histograma:
 plot(density(p.test$x[col==col0]),col=c0,xlim = c(-4,5),ylim = c(0,0.7),lty=2,lwd=4,
      main='Density of first axe factorial',xlab='LDA 1')
@@ -88,6 +95,10 @@ abline(v=corte,lwd=3)
 legend('topleft',legend = c('is_canceled=0','is_canceled=1','puto de referencia'),
        col=c(col0,col1,1),lty=c(2,2,1))
 
+# matriz confusion
+t.test<-f(pes,tabla = T,prob = p.test$posterior[,1])$matrix
+t.train<-f(pes,tabla = T,prob = p.train$posterior[,1],data = d.lda.train$is_canceled)$matrix
 
-
-
+library(caret)
+confusionMatrix(t.test)
+confusionMatrix(t.train)
