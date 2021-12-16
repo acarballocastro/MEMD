0062efc924231e265654b9ac4b359ff03fe779ab
yuhangliang <91143269+yuhangliang@users.noreply.github.com>
2021-12-16 11:12:36 +0100

    Create Associacion.r

---
+++ sintax/Associacion.r
@@ -0,0 +1,25 @@
+library(arulesViz)
+library(arules)
+
+# se hacen las transformaciones necesarias para poder utilizar la función del paquete arules
+dd <- d.e
+dcat <- dd[, as.vector(sapply(dd, is.factor))]
+trans <- as(dcat, "transactions")
+
+# Se buscan las normas que tengan como resultado una cancelación de la reserva.
+# Se filtan las que tengan longitud minima de 10, con un soporte mínimo de 0.2 y una confidence 
+# mínima de 0.4
+# al usar restricciones mas laxas, aparecen miles de reglas, y si se usan restricciones más 
+# fuertes, no aparece ninguna regla.
+
+rules <- apriori(dcat, parameter = list(support = 0.2, confidence = 0.4,  minlen = 10, target= "rules"), appearance = list(default="lhs", rhs="is_canceled=canceled"))
+
+# La función da como resultado 8 reglas
+inspect(rules)
+
+# Esta gráfica muestra la norma con mayor confidence.
+subrules <- head(rules, n = 1, by = "confidence")
+plot(subrules, method = "graph",  engine = "htmlwidget")
+
+# Esta gráfica muestra las 8 reglas obtenidas
+plot(rules, method = "graph",  engine = "htmlwidget")
