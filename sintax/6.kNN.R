2a379d0d5ff043556fc3a0a4fadb8be96abae999
yuhangliang <91143269+yuhangliang@users.noreply.github.com>
2021-12-16 13:01:26 +0100

    Update 6.kNN.R

--- sintax/6.kNN.R
+++ sintax/6.kNN.R
@@ -6,11 +6,26 @@
 # DescripciÃ³n: uso del KNN para la predicciÃ³n.
 ###############################################
 
-#install.packages("VIM")
-library(VIM)
 
-dd_knn<- read.csv("hotel_bookings_proc.csv")
-dd_knn$is_canceled <- as.factor(dd_knn$is_canceled)
+library(VIM)
+library(Rcpp)
+if(!exists('d.e')){
+  path <-'../data'
+  d.e <- read.csv2(paste0(path,'/',"hotel_bookings_proc.csv"), sep=",")
+  d.e$adr<-as.numeric(d.e$adr)
+  v<-list(
+    categoric=c('hotel','is_canceled', 'arrival_date_month','arrival_date_year', 'meal','market_segment','distribution_channel',
+                'is_repeated_guest','reserved_room_type','assigned_room_type', 'room_coherence', 
+                'is_company', 'is_agent', 'customer_type','deposit_type', 'if_prev_cancel','if_wait'),
+    integer=c('lead_time', 'stays_in_weekend_nights','stays_in_week_nights','adults','children','babies',
+              'booking_changes','required_car_parking_spaces','total_of_special_requests'),
+    continua='adr')
+  v$numeric <- c(v$integer,v$continua)
+  library(ggplot2)
+  for(i in v$categoric) d.e[[i]]<-as.factor(d.e[[i]])
+  for(i in v$integer) d.e[[i]]<-as.integer(d.e[[i]])
+}
+dd_knn<-d.e
 
 test <- sample(1:nrow(dd_knn), size = nrow(dd_knn)/3)
 dataTrain <- dd_knn[-test,]
@@ -47,4 +62,5 @@ tab<-table(result[test,2], dd_knn[test,2])
 library(caret)  
 
 confusionMatrix(tab)
+
 
