2c4bdbd714eb0c0ceac420886f4ae4c7ade1c3c8
yuhangliang <91143269+yuhangliang@users.noreply.github.com>
2021-12-16 12:43:36 +0100

    Update 3.Clustering.R

--- sintax/3.Clustering.R
+++ sintax/3.Clustering.R
@@ -10,36 +10,28 @@
 #     - k-means (partitional clustering:  The algorithms require the analyst to specify the number of clusters to be generated.)
 #     - hierarchical clustering
 ###############################################
-# Instalamos y cargamos los paquetes que usaremos
-ipak <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-}
+# cargamos los paquetes que usaremos
 
 packages <- c("tidyverse", "factoextra","NbClust","tidyr","cluster","lattice", "FactoMineR")
-ipak(packages)
-
-#Leemos la base de datos
-path<-strsplit(shell('dir TMD.Rmd',intern = T)[4],' ')[[1]][4]
-filename<- "data_preproc.csv"
-setwd(path)
-dd<-read.csv(paste0('Data/',filename))
-
-# Declaración de variables, no incluimos la variables respuesta 'is_canceled'
-v<-list(
-  categoric=c('hotel','is_canceled', 'arrival_date_month','arrival_date_year', 'meal','market_segment','distribution_channel',
-              'is_repeated_guest','reserved_room_type','assigned_room_type', 'room_coherence', 
-              'is_company', 'is_agent', 'customer_type','deposit_type', 'if_prev_cancel','if_wait'),
-  integer=c('lead_time', 'stays_in_weekend_nights','stays_in_week_nights','adults','children','babies',
-            'booking_changes','required_car_parking_spaces','total_of_special_requests'),
-  continua='adr')
-
-v$numeric <- c(v$integer,v$continua)
-
-for(i in v$categoric) dd[[i]]<-as.factor(dd[[i]])
-for(i in v$integer) dd[[i]]<-as.integer(dd[[i]])
+sapply(packages, require, character.only = TRUE)
+getwd()
+if(!exists('d.e')){
+  path <-'../data'
+  d.e <- read.csv2(paste0(path,'/',"hotel_bookings_proc.csv"), sep=",")
+  d.e$adr<-as.numeric(d.e$adr)
+  v<-list(
+    categoric=c('hotel','is_canceled', 'arrival_date_month','arrival_date_year', 'meal','market_segment','distribution_channel',
+                'is_repeated_guest','reserved_room_type','assigned_room_type', 'room_coherence', 
+                'is_company', 'is_agent', 'customer_type','deposit_type', 'if_prev_cancel','if_wait'),
+    integer=c('lead_time', 'stays_in_weekend_nights','stays_in_week_nights','adults','children','babies',
+              'booking_changes','required_car_parking_spaces','total_of_special_requests'),
+    continua='adr')
+  v$numeric <- c(v$integer,v$continua)
+  library(ggplot2)
+  for(i in v$categoric) d.e[[i]]<-as.factor(d.e[[i]])
+  for(i in v$integer) d.e[[i]]<-as.integer(d.e[[i]])
+}
+dd<-d.e
 
 # 3.1 k-means clustering
 dd.num <- dd[,v$numeric]
